89cc4740813e1ca058e73461e084e0c276ca3a21
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2020-03-10 19:33:14 -0400

    changed deployment settings

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.use('/api/auth/', require('./routes/api/auth'));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
