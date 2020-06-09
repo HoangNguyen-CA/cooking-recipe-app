77728059e604a71097177836868c2ed9fea4de29
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-06-09 18:41:01 -0400

    moved redux store to index.js and integrated redux-dev-extensions

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './store';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 /*
 const script = document.createElement('script');
@@ -19,8 +30,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
